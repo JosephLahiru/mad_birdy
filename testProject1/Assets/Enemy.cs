07e2b0b06193a05c71de6f94a231ea928de79e74
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2021-07-13 00:53:30 +0530

    added killing enemies

--- testProject1/Assets/Enemy.cs
+++ testProject1/Assets/Enemy.cs
@@ -1,6 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Birdy_")
+        {
+            Destroy(gameObject);
+        }
+
+        if (coll.contacts[0].normal.y < -0.5)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
